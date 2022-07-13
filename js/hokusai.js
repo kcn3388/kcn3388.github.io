ea72c9137219c36300556d4ac2a7deb495508693
kcn3388 <kcn3388@126.com>
2022-07-13 17:59:00 +0800

    20220713

--- js/hokusai.js
+++ js/hokusai.js
@@ -1,7 +1,7 @@
 console.log("%c+",
 `font-size: 1px;
 padding: 122px 217px;
-background-image: url(` + root_domain + `/img/default-cover.jpg);
+background-image: url(https://mysql.kcn3388.com/hexo-theme-obsidian/src/img/default-cover.jpg);
 background-size: contain;
 background-repeat: no-repeat;
 color: transparent;`);
